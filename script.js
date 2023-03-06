9ce71f7a088f576a17c02068b3e51dc98c9fe071
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-06 15:27:06 -0500

    - Call menu-icon in script file

---
+++ script.js
@@ -0,0 +1,6 @@
+let menuIcon = document.getElementById("ic");
+let menuItem = document.getElementById("it-nav");
+
+menuIcon.addEventListener("click",()=>{
+    menuItem.classList.toggle("showM");
+})
