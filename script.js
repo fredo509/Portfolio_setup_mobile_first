72f0f0d68ef83f2ec06a0c78ea15a7a10b33ff19
Booze33 <tebetisloh@gmail.com>
2023-03-07 14:08:30 -0600

    Adjust script.js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 let iconMenu = document.querySelector("#ml");
 let menu = document.getElementById("it-nav");
+iconMenu.addEventListener("click", () => {
+    menu.classList.toggle("showMenu");
+    iconMenu.classList.toggle("changeIcon");
+})
