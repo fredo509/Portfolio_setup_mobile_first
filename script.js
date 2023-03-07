70c8ff2c3a64bf76efa36257649b65e615b5bcc1
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-07 17:23:16 -0500

    -Remove nav-list on click on nav-list

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const iconMenu = document.querySelector('#ml');
 const menu = document.getElementById('it-nav');
+const navLink = document.querySelector('.nav-link');
 iconMenu.addEventListener('click', () => {
   menu.classList.toggle('showMenu');
   iconMenu.classList.toggle('changeIcon');
+});
+navLink.addEventListener('click', () => {
+  menu.classList.remove('showMenu');
+  iconMenu.classList.remove('changeIcon');
 });
