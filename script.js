8b80b1046c530c7d062b743fc7fd7ebc8357247d
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-07 15:31:00 -0500

    -Fix linter error

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let iconMenu = document.querySelector("#ml");
-let menu = document.getElementById("it-nav");
-iconMenu.addEventListener("click", () => {
-    menu.classList.toggle("showMenu");
-    iconMenu.classList.toggle("changeIcon");
-})
+const iconMenu = document.querySelector('#ml');
+let menu = document.getElementById('it-nav');
+iconMenu.addEventListener('click', () => {
+  menu.classList.toggle('showMenu');
+  iconMenu.classList.toggle('changeIcon');
+});
