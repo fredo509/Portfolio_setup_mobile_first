3df390a516b0b07f2496632381dcca2701fad656
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-07 15:41:23 -0500

    -Fix linters error

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const iconMenu = document.querySelector('#ml');
-let menu = document.getElementById('it-nav');
+const menu = document.getElementById('it-nav');
 iconMenu.addEventListener('click', () => {
   menu.classList.toggle('showMenu');
   iconMenu.classList.toggle('changeIcon');
