6020071a967d84ff2c576acbb1159f9550531404
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-07 20:08:44 -0500

    -Fix url for closing nav window

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const iconMenu = document.querySelector('#ml');
 const menu = document.getElementById('it-nav');
-const navLink = document.querySelector('.nav-link');
+const navLink = document.querySelectorAll(".nav-list");
 iconMenu.addEventListener('click', () => {
   menu.classList.toggle('showMenu');
   iconMenu.classList.toggle('changeIcon');
 });
-navLink.addEventListener('click', () => {
+navLink.forEach(navUrl => {
+  navUrl.addEventListener('click', () => {
   menu.classList.remove('showMenu');
   iconMenu.classList.remove('changeIcon');
+  });
 });
