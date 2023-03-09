877f4ff47187ae0db68b24e545a109976d3665b4
Fredo St Fleur <fredostfleur23@gmail.com>
2023-03-09 14:00:24 -0500

    -Add js feature and design for mobile

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const iconMenu = document.querySelector('#ml');
 const menu = document.getElementById('it-nav');
 const navLink = document.querySelectorAll('.nav-list');
-const btnSeeP = document.querySelectorAll('.btn-project')
-const btn1 = document.getElementById('btnp');
-const sectionProject = document.getElementById('section-project');
+const btnP = document.querySelectorAll('.btnP');
+const header = document.getElementById('header');
 
 iconMenu.addEventListener('click', () => {
   menu.classList.toggle('showMenu');
@@ -16,24 +15,31 @@ navLink.forEach((navUrl) => {
   });
 });
 
-// mywork section 
-// btnSeeP.forEach((navUrl) => {
-//   navUrl.addEventListener('click', () => {
-//     openPopUp(0);
-//     iconMenu.scrollIntoView();
-//   });
-// });
+//javascript action for  mywork section 
 
-// btn1.addEventListener('click', () => {
-//   openPopUp(0);
-// });
+btnP.forEach((navUrl, index) => {
+  navUrl.addEventListener('click', () => {
+    openPopUp(index);
+    header.scrollIntoView();
+  });
+});
 
-
+// Object Array for details card
 
 const myWork = [{
   name: 'Tonic',
   infoproject: ['Back end Dev', '2015'],
-	image: './images/project_1.svg',
+  image: './images/project_1.svg',
+  projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  proTech: ['html', 'javascript', 'css'],
+  liveUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
+  repoUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
+},
+
+{
+  name: 'Multi-Post Stories',
+  infoproject: ['Back end Dev', '2015'],
+  image: './images/project2.svg',
   projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   proTech: ['html', 'javascript', 'css'],
   liveUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
@@ -43,19 +49,28 @@ const myWork = [{
 {
   name: 'Tonic',
   infoproject: ['Back end Dev', '2015'],
-  image: './images/project_1.svg',
+  image: './images/project3.svg',
   projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
   proTech: ['html', 'javascript', 'css'],
   liveUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
   repoUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
+},
 
- }
-
-
+{
+  name: 'Multi-Post Stories',
+  infoproject: ['Back end Dev', '2015'],
+  image: './images/project2.svg',
+  projectDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+  proTech: ['html', 'javascript', 'css'],
+  liveUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
+  repoUrl: 'https://github.com/fredo509/Portfolio_setup_mobile_first',
+}
 ];
 
+// Action performing when btn see project ScriptProcessorNode
 
 function openPopUp(id) {
+  // create new div with in put it in document body 
 
   const popUpView = document.createElement('div');
   const dataContainer = document.createElement('div');
@@ -63,54 +78,52 @@ function openPopUp(id) {
   popUpView.classList.add('overlay');
   document.body.appendChild(popUpView);
   document.body.appendChild(dataContainer);
-  dataContainer.innerHTML = ` 
-  		<span class="close">
 
-		</span>
-		
-			<div class="pop-card">
-				<div class="project-details">
-					<h2 class="project-title js-title">${myWork[id].name}</h2>
-					<div class="client-info clI">
-						<p class="canopy">CANOPY</p>
-						<ul class="Cinf list">
-							<li>${myWork[id].infoproject[0]}</li>
-							<li>${myWork[id].infoproject[1]}</li>
-						</ul>
-					</div>
-
-				</div>
-
-				<div class="pI1"><img src='${myWork[id].image}' alt = 'projectimage'></div>
-				<div class="pInf">
-					<p>
-						${myWork[id].projectDescription}
-					</p>
-				</div>
-				<div class="">
-					<ul class="">
-						<li>${myWork[id].proTech[0]}</li>
-						<li>${myWork[id].proTech[1]}</li>
-						<li>${myWork[id].proTech[2]}</li>
-					</ul>
-				</div>
-			
-			</div>
+  dataContainer.innerHTML = `
+  <div class="pop-card">
+	<div class="project-details pd">
+		<h2 class="project-title js-title">${myWork[id].name}<span class="closeB"><img src="./images/closeB.svg" alt="closingIcon"></span>
+		</h2>
+		<div class="client-info clI">
+			<p class="canopy">CANOPY</p>
+			<ul class="Cinf list">
+				<li>${myWork[id].infoproject[0]}</li>
+				<li>${myWork[id].infoproject[1]}</li>
+			</ul>
 		</div>
-    
- `;
+	</div>
+	<div class="pI1"><img src='${myWork[id].image}' alt='projectimage'></div>
+	<div class="pInf">
+		<p>
+			${myWork[id].projectDescription}
+		</p>
+	</div>
+	<div class="p-tag">
+		<ul class="pl plp">
+			<li>${myWork[id].proTech[0]}</li>
+			<li>${myWork[id].proTech[1]}</li>
+			<li>${myWork[id].proTech[2]}</li>
+		</ul>
+	</div>
+	<div class="redirect">
+		<a class="myBtns" href='${myWork[id].liveUrl}'>
+			See live
+			<img src="./images/seeL.svg" alt="">
+		</a>
+		<a class="myBtns" href="${myWork[id].repoUrl}">
+			See source
+			<img src="./images/seeS.svg" alt="">
+		</a>
+	</div>
+</div>
+</div>
+`;
+  // Action for closing popUp window
+  const hideWin = dataContainer.querySelectorAll('.closeB');
+  hideWin.forEach((closeMe) => {
+    closeMe.addEventListener('click', ()=>{
+      popUpView.style.display = "none";
+      dataContainer.style.display = "none";
+    });
+  });
 }
-openPopUp(0);
-
-
-// function showPopUp(projectNumber) {
-
-//   document.addEventListener('click', (action) => {
-//     if (action.target.id === 'close-popUp') {
-//       popUpView.style.display = 'none';
-//       overlayView.style.display = 'none';
-//     }
-//   });
-// }
-// // export the function to use after rendering the projects dynamically
-// export { showPopUp as default };
